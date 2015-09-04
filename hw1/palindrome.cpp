f9e1344f709f0c18b199251182126fc89ed998a4
Caio Vinicius Marques Teixeira <marquest@usc.edu>
2015-09-03 23:44:46 -0700

    Problem 7

--- hw1/palindrome.cpp
+++ hw1/palindrome.cpp
@@ -5,7 +5,46 @@
 
 using namespace std;
 
-void makePalindromes(ostream& ofile, char* options, int size);
+void makePalindromesHelper(ostream& ofile, char* options, int size, string pal)
+{
+
+	string reversePal = string(pal.rbegin(), pal.rend());
+	
+	//Size of options
+	string str(options);
+	int optionsSize = str.size();
+
+	//Base cases
+	if(size == 0)
+	{
+		pal = pal + "" + reversePal;
+		ofile << pal << endl;
+	}
+	else if(size == 1)
+	{
+		for(int i = 0; i < optionsSize; i++){
+			string out = pal + options[i] + reversePal;
+			ofile << out << endl;
+		}
+	}
+	// xYx, where Y is palindrome and x is a character
+	else if (size > 1)
+	{
+		for(int i = 0; i < optionsSize; i++){
+			string out = pal + options[i];
+			makePalindromesHelper(ofile, options, size-2, out);
+		}
+	}
+}
+
+void makePalindromes(ostream& ofile, char* options, int size)
+{
+	for(int i = 1; i <= size; i++)
+	{
+		makePalindromesHelper(ofile, options, i, "");
+	}
+
+}
 
 
 int main(int argc, char* argv[])
@@ -15,7 +54,8 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  ofstream ofile(argv[1]);
+  ofstream ofile;
+  ofile.open(argv[1]);
   int len = atoi(argv[3]);
   makePalindromes(ofile, argv[2], len);
   ofile.close();
